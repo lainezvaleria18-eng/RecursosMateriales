e0f0baf6ed1c205a2cba4adf0580ce7511d40442
Kevin Ernesto Meza Garcia <erasmomeza11@gmail.com>
2026-05-29 18:34:36 -0600

    refactor-fix

--- src/test/java/esfe/persistencia/CategoriaMaterialDAOTest.java
+++ src/test/java/esfe/persistencia/CategoriaMaterialDAOTest.java
@@ -1,28 +1,12 @@
 package esfe.persistencia;
 
-import esfe.dominio.CategoriaMaterial;
+import org.junit.jupiter.api.Test;
 
-public class CategoriaMaterialDAOTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    public static void main(String[] args) {
+class CategoriaMaterialDAOTest {
 
-        CategoriaMaterial categoria = new CategoriaMaterial();
-
-        categoria.setNombreCategoria("Mouse");
-
-        categoria.setDescripcion("Mouse inalámbrico");
-
-        CategoriaMaterialDAO dao = new CategoriaMaterialDAO();
-
-        int resultado = dao.guardar(categoria);
-
-        if (resultado > 0) {
-
-            System.out.println("Guardado correctamente");
-
-        } else {
-
-            System.out.println("Error al guardar");
-        }
+    @Test
+    void guardar() {
     }
 }
--- src/test/java/esfe/persistencia/RolDAOTest.java
+++ src/test/java/esfe/persistencia/RolDAOTest.java
@@ -1,33 +1,12 @@
 package esfe.persistencia;
 
-import esfe.dominio.Rol;
+import org.junit.jupiter.api.Test;
 
-public class RolDAOTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    public static void main(String[] args) {
+class RolDAOTest {
 
-        try {
-
-            Rol rol = new Rol();
-
-            rol.setNombreRol("Supervisor");
-
-            RolDAO dao = new RolDAO();
-
-            int resultado = dao.guardar(rol);
-
-            if (resultado > 0) {
-
-                System.out.println("Registro guardado correctamente");
-
-            } else {
-
-                System.out.println("No se guardó");
-            }
-
-        } catch (Exception e) {
-
-            System.out.println("Error: " + e.getMessage());
-        }
+    @Test
+    void guardar() {
     }
 }
