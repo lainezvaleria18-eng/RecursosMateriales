d95467cab7f92e71cee529ab2cc3b1b5db4543ec
Daniela Campos <danielaroblox1617@gmail.com>
2026-05-29 18:23:57 -0600

    fix(EstadosRecursos): correción de código del test de EstadosRecusos, Daniela Tatiana Campos Mancia

--- src/test/java/esfe/persistencia/EstadosRecursosDAOTest.java
+++ src/test/java/esfe/persistencia/EstadosRecursosDAOTest.java
@@ -1,16 +1,40 @@
 package esfe.persistencia;
 
+import esfe.dominio.EstadosRecursos;
 import org.junit.jupiter.api.Test;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EstadosRecursosDAOTest {
 
     @Test
     void insertar() {
+
+        EstadosRecursos nuevoEstado = new EstadosRecursos();
+        nuevoEstado.setNombreEstado("Mantenimiento");
+
+
+        EstadosRecursosDAO dao = new EstadosRecursosDAO();
+
+
+        boolean resultado = dao.insertar(nuevoEstado);
+
+
+        assertTrue(resultado, "No se pudo insertar el estado del recurso.");
     }
 
     @Test
     void obtenerTodos() {
+
+        EstadosRecursosDAO dao = new EstadosRecursosDAO();
+
+
+        List<EstadosRecursos> lista = dao.obtenerTodos();
+
+
+        assertNotNull(lista, "La lista regresó como un valor nulo.");
+
+
+        System.out.println("Estados encontrados en la BD: " + lista.size());
     }
 }
