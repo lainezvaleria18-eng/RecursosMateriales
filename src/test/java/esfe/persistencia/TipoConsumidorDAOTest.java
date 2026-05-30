59b2b771123e87ba694b45bca3c735b925a76c5b
Daniela Campos <danielaroblox1617@gmail.com>
2026-05-30 13:57:49 -0600

    feat se agreg persistencia ala tabla TipoConsumidor- KevinMeza

--- src/test/java/esfe/persistencia/TipoConsumidorDAOTest.java
+++ src/test/java/esfe/persistencia/TipoConsumidorDAOTest.java
@@ -1,12 +1,24 @@
+
 package esfe.persistencia;
 
+import esfe.dominio.TipoConsumidor;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TipoConsumidorDAOTest {
 
     @Test
     void guardar() {
+
+        TipoConsumidor tipo = new TipoConsumidor();
+        tipo.setNombreEspecialidad("Prueba");
+
+        TipoConsumidorDAO dao = new TipoConsumidorDAO();
+
+        int resultado = dao.guardar(tipo);
+
+        assertEquals(1, resultado);
     }
 }
+
