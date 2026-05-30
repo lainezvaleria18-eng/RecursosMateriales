45b3d71829dab35bd90399bad3a7dbf5cb8165fe
Daniela Campos <danielaroblox1617@gmail.com>
2026-05-30 14:05:11 -0600

    feat se agreg persistencia ala tabla Rol - KevinMeza

--- src/test/java/esfe/persistencia/CategoriaMaterialDAOTest.java
+++ src/test/java/esfe/persistencia/CategoriaMaterialDAOTest.java
@@ -1,12 +1,24 @@
+
 package esfe.persistencia;
 
+import esfe.dominio.CategoriaMaterial;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CategoriaMaterialDAOTest {
 
     @Test
     void guardar() {
+
+        CategoriaMaterial categoria = new CategoriaMaterial();
+        categoria.setNombreCategoria("Prueba");
+        categoria.setDescripcion("Descripcion de prueba");
+
+        CategoriaMaterialDAO dao = new CategoriaMaterialDAO();
+
+        int resultado = dao.guardar(categoria);
+
+        assertEquals(1, resultado);
     }
 }
