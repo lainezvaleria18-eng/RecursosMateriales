301daf2bfea85a5befb87ff0357b3d6b25ff0e9e
Daniela Campos <danielaroblox1617@gmail.com>
2026-05-30 13:51:21 -0600

    feat se agreg persistencia ala tabla Rol - KevinMeza

--- src/test/java/esfe/persistencia/RolDAOTest.java
+++ src/test/java/esfe/persistencia/RolDAOTest.java
@@ -1,12 +1,22 @@
 package esfe.persistencia;
 
+import esfe.dominio.Rol;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RolDAOTest {
 
     @Test
     void guardar() {
+
+        Rol rol = new Rol();
+        rol.setNombreRol("RolPrueba");
+
+        RolDAO dao = new RolDAO();
+
+        int resultado = dao.guardar(rol);
+
+        assertEquals(1, resultado);
     }
 }
