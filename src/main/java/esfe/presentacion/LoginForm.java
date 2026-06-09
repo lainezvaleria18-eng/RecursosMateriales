432e0261bcd4a16de88c6e86d6557fe5e884139e
valer <lainezvaleria18@gmail.com>
2026-06-09 14:29:46 -0600

    feat(presentacion): diseñar interfaz del Login y estructurar formulario de registro , Monica Valeria Ruballo Lainez

---
+++ src/main/java/esfe/presentacion/LoginForm.java
@@ -0,0 +1,13 @@
+package esfe.presentacion;
+
+import javax.swing.*;
+
+public class LoginForm extends JDialog {
+    private JPanel mainpanel;
+    private JPanel cardpanel;
+    private JTextField textUsuario;
+    private JPasswordField passwordField1;
+    private JButton btnRecuperar;
+    private JButton btnCrearCuenta;
+    private JButton btnIngresar;
+}
