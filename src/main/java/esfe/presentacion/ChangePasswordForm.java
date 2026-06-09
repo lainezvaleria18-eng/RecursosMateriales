cecd9208d7651eaac694b526cd582313c21dac55
valer <lainezvaleria18@gmail.com>
2026-06-09 15:58:57 -0600

    feat(presentacion): completar diseño y Crear Cuenta , Monica Valeria Ruballo Lainez

--- src/main/java/esfe/presentacion/ChangePasswordForm.java
+++ src/main/java/esfe/presentacion/ChangePasswordForm.java
@@ -3,4 +3,13 @@ package esfe.presentacion;
 import javax.swing.*;
 
 public class ChangePasswordForm extends JDialog {
+    private JPanel mainpanel;
+    private JPanel cardpanel;
+    private JTextField txtUsuario;
+    private JTextField txtCorreo;
+    private JPasswordField txtConfirmarClave;
+    private JPasswordField txtClave;
+    private JButton btnRegistrar;
+    private JButton yaTienesCuentaButton;
+    private JButton btnRegresarLogin;
 }
--- src/main/java/esfe/presentacion/LoginForm.java
+++ src/main/java/esfe/presentacion/LoginForm.java
@@ -6,7 +6,7 @@ public class LoginForm extends JDialog {
     private JPanel mainpanel;
     private JPanel cardpanel;
     private JTextField textUsuario;
-    private JPasswordField passwordField1;
+    private JPasswordField txtClave;
     private JButton btnRecuperar;
     private JButton btnCrearCuenta;
     private JButton btnIngresar;
