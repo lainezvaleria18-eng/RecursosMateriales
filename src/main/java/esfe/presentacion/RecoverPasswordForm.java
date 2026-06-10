969e603c76ad40d43fc31c38a1ca6380e415e669
valer <lainezvaleria18@gmail.com>
2026-06-10 12:38:21 -0600

    feat(presentacion): crear formulario RecoverPasswordForm para recuperación de contraseña, Monica Valeria Ruballo Lainez

---
+++ src/main/java/esfe/presentacion/RecoverPasswordForm.java
@@ -0,0 +1,10 @@
+package esfe.presentacion;
+
+import javax.swing.*;
+
+public class RecoverPasswordForm  extends JDialog {
+    private JPanel mainpanel;
+    private JTextField txtCorreoRecuperar;
+    private JButton btnEnviarEnlace;
+    private JButton btnVolverLogin;
+}
--- src/main/java/esfe/presentacion/UserWriteForm.java
+++ src/main/java/esfe/presentacion/UserWriteForm.java
@@ -3,4 +3,13 @@ package esfe.presentacion;
 import javax.swing.*;
 
 public class UserWriteForm  extends JDialog {
+    private JPanel mainpanel;
+    private JPanel cardpanel;
+    private JTextField textUsuario;
+    private JTextField textcorreo;
+    private JPasswordField txtclave;
+    private JPasswordField txtconfirmarclave;
+    private JButton btnRegistrar;
+    private JButton yaTienesCuentaButton;
+    private JButton btnRegresarLogin;
 }
