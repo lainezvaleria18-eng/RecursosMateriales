2e3d0efa43226d8e344bfa4060d2b22aab5d589c
valer <lainezvaleria18@gmail.com>
2026-06-10 22:42:50 -0600

    feat(presentacion): crear y diseñar la interfaz PasswordSuccessForm, Monica Valeria Ruballo Lainez

---
+++ src/main/java/esfe/presentacion/PasswordSuccessForm.java
@@ -0,0 +1,9 @@
+package esfe.presentacion;
+
+import javax.swing.*;
+
+public class PasswordSuccessForm extends JDialog {
+    private JPanel mainpanel;
+    private JPanel cardpanel;
+    private JButton btnIniciarSesion;
+}
