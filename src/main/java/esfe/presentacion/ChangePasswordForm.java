07283cf7a110974b131286dbfd839a01cea238e8
valer <lainezvaleria18@gmail.com>
2026-06-10 18:42:21 -0600

    feat(presentacion): crear y diseñar la interfaz ChangePasswordForm, Monica Valeria Ruballo Lainez

---
+++ src/main/java/esfe/presentacion/ChangePasswordForm.java
@@ -0,0 +1,11 @@
+package esfe.presentacion;
+
+import javax.swing.*;
+
+public class ChangePasswordForm {
+    private JPanel mainpanel;
+    private JPanel cardpanel;
+    private JTextField txtNuevaContrasena;
+    private JTextField txtConfirmarContrasena;
+    private JButton btnGuardarContrasena;
+}
--- src/main/java/esfe/presentacion/VerifyCodeForm.java
+++ src/main/java/esfe/presentacion/VerifyCodeForm.java
@@ -5,12 +5,13 @@ import javax.swing.*;
 public class VerifyCodeForm extends JDialog {
     private JPanel mainpanel;
     private JPanel cardpanel;
-    private JButton verificarCodigoButton;
-    private JButton reenviarCodigoButton;
+    private JButton btnverificarcodigo;
+    private JButton btnReenviarCodigo;
     private JTextField txtCod1;
     private JTextField txtCod2;
     private JTextField txtCod3;
     private JTextField txtCod4;
     private JTextField txtCod5;
     private JPasswordField txtCod6;
+    private JLabel Verificar;
 }
